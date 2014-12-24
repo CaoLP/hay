19adbf63abd5f46ca42288ecdcdd2ef2a7c25aa6
lpcao <caolpgc60067@gmail.com>
2014-12-24 17:27:32 +0700

    new update

---
+++ webroot/services/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <script type='text/javascript'>
+        var googletag = googletag || {};
+        googletag.cmd = googletag.cmd || [];
+        (function() {
+            var gads = document.createElement('script');
+            gads.async = true;
+            gads.type = 'text/javascript';
+            var useSSL = 'https:' == document.location.protocol;
+            gads.src = (useSSL ? 'https:' : 'http:') +
+                '//www.googletagservices.com/tag/js/gpt.js';
+            var node = document.getElementsByTagName('script')[0];
+            node.parentNode.insertBefore(gads, node);
+        })();
+    </script>
+
+    <script type='text/javascript'>
+        googletag.cmd.push(function() {
+            googletag.defineSlot('/94128653/ht326x280', [336, 280], 'div-gpt-ad-1419411078489-0').addService(googletag.pubads());
+            googletag.pubads().enableSingleRequest();
+            googletag.enableServices();
+        });
+    </script>
+</head>
+<body>
+<!-- ht326x280 -->
+<div id='div-gpt-ad-1419411078489-0' style='width:336px; height:280px;'>
+    <script type='text/javascript'>
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1419411078489-0'); });
+    </script>
+</div>
+</body>
+</html>
